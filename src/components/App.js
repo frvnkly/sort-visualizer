833a71c619c6a6f43fbf646ffa72ab018a6c98dc
Frank Li <li.26.frank@gmail.com>
2020-05-21 23:54:17 -0400

    initial commit with tailwindcss

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../tailwind.generated.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container mx-auto mt-8 text-center'>
+        <h1 className='font-mono text-3xl'>Sort Visualizer</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
